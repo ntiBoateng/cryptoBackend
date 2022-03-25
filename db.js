c7d8655203d2076d8a5013d875713651b7ea7623
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:26:02 -0800

    index.js files setup

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+//const mongoURI = "mongodb+srv://Pankaj:{shshsh}2@cluster0.uehfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://bgnti:Aladdin200@cryptocluster.rmdtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected")
+    })
+}
+
+module.exports = connectToMongo;
