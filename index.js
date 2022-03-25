47dd3e652a7fda654d857f73607142dd035184f7
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:12:56 -0800

    index.js files setup

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ const portfolioRoute = require('./routes/Portfolio')
 const watchlistRoute = require('./routes/Watchlist')
 
 connectToMongo();
-
+app.get("/",(req,res)=>{
+    res.send("Express Backendbthis is right behind you !")
+  })
+  app.use('/api/auth', authRoute)
+  app.use('/api/portfolio', portfolioRoute)
+  app.use('/api/watchlist', watchlistRoute)
+  
+  
