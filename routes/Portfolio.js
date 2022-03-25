b5307c33bb59cf3a1344dccdb1d10e07e60f822c
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:57:39 -0800

    Portfolio routes

--- routes/Portfolio.js
+++ routes/Portfolio.js
@@ -14,3 +14,28 @@ router.get('/fetchallportfolios', fetchuser, async (req, res) => {
         res.status(500).send("Some Error Occurred")
     }
 })
+
+ROUTE 2: Add a new Portfolio using: POST "/api/auth/addportfolio". Login required
+router.post('/addportfolio', fetchuser, async (req, res) => {
+        try {
+
+            const { coinid, amount } = req.body;
+
+            // If there are errors, return Bad request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const portfolio = new Portfolio({
+                coinid, amount, user: req.user.id
+            })
+            const savedPortfolio = await portfolio.save()
+
+            res.json(savedPortfolio)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+})
+ 
