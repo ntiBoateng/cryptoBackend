496b1cbaa05cd8be43b1b605683b4cb5f9e2406e
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:48:01 -0800

    authentication setup

--- routes/auth.js
+++ routes/auth.js
@@ -51,6 +51,52 @@ router.post('/createuser', [
     }
 })
 
+//Route 2: Authenticate a user using POST "/api/auth/login". No login required
+// ROUTE 2: Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+  ], async (req, res) => {
+    let success = false;
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        success = false
+        return res.status(400).json({ error: "Please try to login with correct credentials" });
+      }
+  
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        success = false
+        return res.status(400).json({ success, error: "Please try to login with correct credentials" });
+      }
+  
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, Jwt_SECRET);
+      success = true;
+      res.json({ success, authtoken })
+  
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  
+  
+  });
+  
+
+//Route 3 : Authenticate a user using POST "/api/auth/login". No login required
 
 router.post('/getuser', fetchuser, async (req, res) => {
     try {
@@ -61,3 +107,10 @@ router.post('/getuser', fetchuser, async (req, res) => {
         res.status(500).send("Some Error Occurred")
     }
 }) 
+
+
+
+
+
+
+module.exports = router
