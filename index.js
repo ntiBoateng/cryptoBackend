6ae3f0171e231c43beae47e98183ce320246b472
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:09:58 -0800

    index.js files setup

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const connectToMongo = require('./db')
 require('dotenv').config()
 const express = require('express')
 
+const authRoute = require('./routes/auth')
+const portfolioRoute = require('./routes/Portfolio')
+const watchlistRoute = require('./routes/Watchlist')
+
+connectToMongo();
