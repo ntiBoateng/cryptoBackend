406593c4ceaebaf393886f5885fa82bee6436e44
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:40:38 -0800

    authentication setup

--- routes/auth.js
+++ routes/auth.js
@@ -7,3 +7,46 @@ var fetchuser = require('../middleware/fetchuser')
 const { body, validationResult } = require('express-validator');
 
 const Jwt_SECRET = "PANKAJ CHAUDHARY";
+
+//Route 1: Create a user using :POST "/api/auth".Doesnt require Auth
+router.post('/createuser', [
+    body('name','invalid name').isLength({ min: 3 }),
+    body('email','invalid email').isEmail(),
+    body('password','too short').isLength({ min: 5 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({error:'Already Registered | Login to Continue'})
+        }
+
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hashSync(req.body.password, salt);
+        user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        });
+        const data = {
+            user: {
+                id:user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, Jwt_SECRET);
+        // console.log(auth-token)
+
+
+        res.json({ authtoken })
+    }
+
+    catch (error) {
+        res.status(500).send("Some Error Occurred")
+    }
+})
