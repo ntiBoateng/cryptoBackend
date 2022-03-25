8e6ac8533954d9209fb756bf1c64fb9dd872b778
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 15:54:07 -0800

    Portfolio routes

--- routes/Portfolio.js
+++ routes/Portfolio.js
@@ -3,3 +3,14 @@ const router = express.Router();
 var fetchuser = require('../middleware/fetchuser')
 const Portfolio = require('../Models/Portfolio')
 const { body, validationResult } = require('express-validator');
+
+router.get('/fetchallportfolios', fetchuser, async (req, res) => {
+
+    try {
+        const portfolios = await Portfolio.find({ user: req.user.id });
+        res.json(portfolios)
+
+    } catch (error) {
+        res.status(500).send("Some Error Occurred")
+    }
+})
