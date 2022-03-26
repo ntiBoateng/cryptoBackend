b95bced6c84ed4ac485ad111413d3c2a79796d64
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 16:07:33 -0800

    Watchlist routes

--- routes/Watchlist.js
+++ routes/Watchlist.js
@@ -13,3 +13,27 @@ router.get('/fetchallwatchlists', fetchuser, async (req, res) => {
         res.status(500).send("Some Error Occurred")
     }
 })
+
+// ROUTE 2: Add a new Watchlist using: POST "/api/auth/addwatchlist". Login required
+router.post('/addwatchlist', fetchuser, async (req, res) => {
+    try {
+
+        const { coinid } = req.body;
+
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const watchlist = new Watchlist({
+            coinid,user: req.user.id
+        })
+        const savedWatchlist = await watchlist.save()
+
+        res.json(savedWatchlist)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
